ae06ea6115b6b5adc94cbb44c059e8091032274a
msmel01 <ms.elly17.01@gmail.com>
2021-08-20 14:02:26 -0400

    added base files

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once('ready', () => {
+	console.log('connected to server!');
+});
+
+client.login(process.env.TOKEN);

4f1bfee5e66ce2337c7fef10e1bfbb3a83d60315
msmel01 <ms.elly17.01@gmail.com>
2021-08-21 07:51:54 -0400

    modified to update the timer object with the work and break periods

--- commands/break.js
+++ commands/break.js
@@ -7,7 +7,13 @@ module.exports = {
 		.addIntegerOption(option => option.setName('time').setDescription('Number of minutes.').setRequired(true)),
 	async execute(interaction) {
 		const time = interaction.options.getInteger('time');
-		interaction.client.break_time = time;
-		await interaction.reply(`${interaction.user} set break period to ${time} minutes.`);
+		if (!interaction.client.timer.running) { // change work time if timer is not running
+			interaction.client.timer.break_time = time;
+			interaction.client.timer.break_time_remaining = time * 60000; // in milliseconds
+			await interaction.reply(`${interaction.user} set break period to ${time} minutes.`);
+		} else {
+			await interaction.reply(`Pause the timer and try again.`);
+		}
+		
 	},
 };
--- commands/work.js
+++ commands/work.js
@@ -7,7 +7,12 @@ module.exports = {
 		.addIntegerOption(option => option.setName('time').setDescription('Number of minutes.').setRequired(true)),
 	async execute(interaction) {
 		const time = interaction.options.getInteger('time');
-		interaction.client.work_time = time;
-		await interaction.reply(`${interaction.user} set work period to ${time} minutes.`);
+		if (!interaction.client.timer.running) { // change work time if timer is not running
+			interaction.client.timer.work_time = time;
+			interaction.client.timer.work_time_remaining = time * 60000; // in milliseconds
+			await interaction.reply(`${interaction.user} set work period to ${time} minutes.`);
+		} else {
+			await interaction.reply(`Pause the timer and try again.`);
+		}
 	},
 };

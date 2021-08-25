ed83970de94369e1a459b1d13c39a427ff39ec6b
msmel01 <ms.elly17.01@gmail.com>
2021-08-25 09:16:01 -0400

    added command handler

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 require('dotenv').config();
 
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const Timer = require('./util/timer');
+
+const { Client, Intents, Collection} = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.once('ready', () => {
 	console.log('connected to server!');
+    client.user.setActivity('/start', {type: 'LISTENING'})
+});
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// 50 minute work time and 10 minute break by default.
+client.timer = new Timer(client, 1, 1);
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	if (interaction.user.bot) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
 client.login(process.env.TOKEN);
